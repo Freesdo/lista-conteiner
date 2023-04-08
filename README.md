87795de35613c4ad11b3956753a9c19b2114bc49
Freesdo <fredericofabricio5@gmail.com>
2023-04-08 15:02:32 -0300

    lista v1.1 removed lista.cpp

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 ## Primeira versão da lista que estou fazendo a partir de ponteiros inteligentes
 
-para incluir a lista, basta incluir o lista.hpp e lista.cpp no seu diretório, crie um arquivo objeto de lista.cpp e inclua #include "lista.hpp" no cabeçalho do seu código
-
-## Exemplo:
-
-```g++ -o lista lista.cpp```
-
+para incluir a lista, basta incluir o lista.hpp no seu diretório do projeto e inclua a lista no cabeçalho do seu código
 ```c++
 #include "lista.hpp"
 ```
@@ -19,12 +14,12 @@ mop::lista<T> MinhaLista;
 ```
 os métodos disponíveis até então são de:
 
-```c++
+```
 MinhaLista.push_back(T dado); <-> retorna void e adiciona um elemento no final da lista
-MinhaLista.pop_back(T dado); <-> retorna um bool referente a se excluiu o elemento procurado (pop_back deveria ser o nome para excluir o último, no proximo commit eu arrumo)
+MinhaLista.erase(T dado); <-> retorna um bool referente a se excluiu o elemento procurado
 MinhaLista.vazia(); <-> retorna um bool referente a se está vazia ou não 
 MinhaLista.busca(T achado); <-> retorna um std::size_t da posição do valor procurado
 MinhaLista.getlista(); <-> retorna void mas imprime toda a lista
 ```
 
-# ainda está em fase de testes e implementações, estou fazendo isso como um passatempo e realmente sei que ninguém vai usar porque já existe <vector> ou outros tipos de contâiners mais práticos e fáceis
+ ainda está em fase de testes e implementações, estou fazendo isso como um passatempo e realmente sei que ninguém vai usar porque já existe <vector> ou outros tipos de contâiners mais práticos e fáceis
--- lista.hpp
+++ lista.hpp
@@ -1,39 +1,87 @@
-#ifndef LISTA_H_
-#define LISTA_H_
+#ifndef LISTAS_H_
+#define LISTAS_H_
 
 #include <iostream>
 #include <memory>
+#include <string>
 
+// adaptar esse tipo de estrutura para qualquer tipo utilizando um template,
+// como um vector
 namespace mof {
 template <typename T> class node {
 public:
-  node(T dado);
-  node();
-  void setprox(std::shared_ptr<node<T>> prox);
-  std::shared_ptr<node<T>> getprox() const;
-  T getdado() const;
-  T get_prox_dado() const;
-  void desalloc();
+  node(T dado) : dado_(dado), prox_(nullptr) {}
+  node() { prox_ = nullptr; }
+  void setprox(std::shared_ptr<node<T>> prox) { prox_ = prox; }
+  std::shared_ptr<node<T>> getprox() const { return prox_; }
+  T getdado() const { return dado_; }
+  T get_prox_dado() const { return prox_->getdado(); }
+  void desalloc() { prox_.reset(); }
 
 private:
   T dado_;
+
   std::shared_ptr<node<T>> prox_;
 };
 
 template <typename T> class lista {
 public:
-  lista();
-  void getlista() const;
-  void getfirst() const;
-  std::size_t busca(T dado) const;
-  bool vazia();
-  void push_back(T element);
-  bool pop_back(T achado);
-  void insert(T element, std::size_t position);
+  lista() : inicio(nullptr) {}
+  void getlista() const {
+    std::shared_ptr<node<T>> it(inicio);
+    while (it) {
+      std::cout << it->getdado() << '\n';
+      it = it->getprox();
+    }
+  }
+  void getfirst() const {
+    std::cout << inicio->getdado();
+  } // estou usando para testes.
+  size_t busca(T dado) const {
+    std::shared_ptr<node<T>> it = inicio;
+    size_t count{};
+    while (it->getdado() != dado) {
+      count++;
+      it = it->getprox();
+    }
+    return count;
+  }
+  bool vazia() const { return inicio ? false : true; }
+  void push_back(T element) {
+    std::shared_ptr<node<T>> novo{std::make_shared<node<T>>(element)};
+    if (vazia()) {
+      inicio = std::move(novo);
+    }
+    std::shared_ptr<node<T>> it(inicio);
+    while (it->getprox()) {
+      it = it->getprox();
+    }
+    it->setprox(novo);
+  }
+  bool erase(T achado) {
+    if (vazia()) {
+      throw std::runtime_error("Lista vazia");
+      return false;
+    }
+    if (inicio->getdado() == achado) {
+      inicio.reset();
+      return true;
+    }
+    std::shared_ptr<node<T>> it = inicio;
+    while (it->getdado() != achado) {
+      it = it->getprox();
+    }
+    std::shared_ptr<node<T>> anterior = inicio;
+    while (anterior->getprox() != it) {
+      anterior = anterior->getprox();
+    }
+    anterior->setprox(it->getprox());
+    return true;
+  }
+  void insert(size_t position); // a implementar ainda
 
 private:
   std::shared_ptr<node<T>> inicio;
 };
 } // namespace mof
-
 #endif
