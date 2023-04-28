e0087a2e88ecd804299add95bea4399997b9fb78
Freesdo <fredericofabricio5@gmail.com>
2023-04-27 22:39:55 -0300

    implementação do método 'insert'

--- lista.hpp
+++ lista.hpp
@@ -1,9 +1,13 @@
 #ifndef LISTAS_H_
 #define LISTAS_H_
 
+#include <exception>
 #include <iostream>
 #include <memory>
+#include <new>
+#include <stdexcept>
 #include <string>
+#include <typeinfo>
 
 // adaptar esse tipo de estrutura para qualquer tipo utilizando um template,
 // como um vector
@@ -26,7 +30,8 @@ private:
 
 template <typename T> class lista {
 public:
-  lista() : inicio(nullptr) {}
+  lista() : inicio(nullptr), tamanho(0) {}
+  std::size_t size() const { return tamanho; }
   void getlista() const {
     std::shared_ptr<node<T>> it(inicio);
     while (it) {
@@ -34,9 +39,6 @@ public:
       it = it->getprox();
     }
   }
-  void getfirst() const {
-    std::cout << inicio->getdado();
-  } // estou usando para testes.
   size_t busca(T dado) const {
     std::shared_ptr<node<T>> it = inicio;
     size_t count{};
@@ -48,7 +50,20 @@ public:
   }
   bool vazia() const { return inicio ? false : true; }
   void push_back(T element) {
-    std::shared_ptr<node<T>> novo{std::make_shared<node<T>>(element)};
+    if (typeid(element).name() != typeid(T).name()) {
+      throw std::bad_cast();
+      return;
+    }
+    std::shared_ptr<node<T>> novo;
+    try {
+      novo = std::make_shared<node<T>>(element);
+    } catch (const std::bad_alloc &exept) {
+      std::cerr << "erro ao alocar memoria para um novo elemento.\n what():\t"
+                << "--" << exept.what() << "--\n"
+                << "sugestao: exclua alguns elementos ou libere memoria para "
+                   "prosseguir novamente\n";
+      return;
+    }
     if (vazia()) {
       inicio = std::move(novo);
     }
@@ -57,6 +72,7 @@ public:
       it = it->getprox();
     }
     it->setprox(novo);
+    tamanho++;
   }
   bool erase(T achado) {
     if (vazia()) {
@@ -76,12 +92,49 @@ public:
       anterior = anterior->getprox();
     }
     anterior->setprox(it->getprox());
+    tamanho--;
     return true;
   }
-  void insert(size_t position); // a implementar ainda
+  void insert(T element, std::size_t position) {
+    if (typeid(T).name() != typeid(element).name()) {
+      throw std::bad_cast();
+      return;
+    }
+    if (vazia() || position > tamanho) {
+      std::cerr << "nao foi possivel inserir o elemento na lista.\n--possiveis "
+                   "causas: lista vazia ou posicao invalida--\n";
+      throw std::out_of_range("posicao invalida");
+      return;
+    }
+    std::shared_ptr<node<T>> novo;
+    try {
+      novo = std::make_shared<node<T>>(element);
+    } catch (const std::bad_alloc exept) {
+      std::cerr
+          << "erro de alocacao de memoria para novo elemento. \n"
+          << "what():\t--" << exept.what() << "--\n"
+          << "tente excluir elementos ou liberar a memoria para prosseguir\n";
+      return;
+    }
+    if (!position) {
+      novo.setprox(inicio);
+      inicio = std::move(novo);
+      tamanho++;
+      return;
+    }
+    std::shared_ptr<node<T>> it(inicio);
+    for (std::size_t i{0}; i < position - 1; ++i) {
+      it = it.getprox();
+    }
+    novo.setprox(it->getprox());
+    it.setprox(novo);
+    tamanho++;
+    return;
+  }
 
 private:
   std::shared_ptr<node<T>> inicio;
+  std::size_t tamanho;
 };
 } // namespace mof
 #endif
